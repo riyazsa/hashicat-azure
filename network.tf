feb8610f617fe0a9e68581b3c9db053dd09b5922
Riyaz <abdulriyaz.shaik@coles.com.au>
2022-03-25 02:23:28 +0000

    Added resource group name

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/riyaz-coles/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "riyaz-network-1"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
