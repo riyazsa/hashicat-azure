3549116889e55836f68ea2c15c62a6fe260ec9c6
Riyaz <abdulriyaz.shaik@coles.com.au>
2022-03-25 01:39:00 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -107,9 +107,11 @@ resource "azurerm_virtual_machine" "catapp" {
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
+  Department          = "devops"
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
+
 
   storage_image_reference {
     publisher = var.image_publisher
@@ -134,7 +136,7 @@ resource "azurerm_virtual_machine" "catapp" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
+  
   tags = {}
 
   # Added to allow destroy to work correctly.
